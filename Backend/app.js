4bce8d35a7da7f76689cbf650134854cff2e4d60
NIYOBYOSE Isaac Precieux <isaac@Isaac>
2025-05-19 21:46:04 +0100

    Created new files

--- Backend/app.js
+++ Backend/app.js
@@ -2,3 +2,13 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+}
+);
